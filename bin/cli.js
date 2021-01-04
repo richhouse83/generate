004db1d79abae04ff740fac9f4e6c6f0278bbb25
Rich House <rich@richhouse.co.uk>
2021-01-04 19:27:22 +0000

    server generator created

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,18 @@
 #!/usr/bin/env node
 
 const projectGenerator = require("../lib/project-generator");
+const serverGenerator = require("../lib/server-generator");
 
 const [, , ...args] = process.argv;
 
-projectGenerator(args[0], (err) => {
-  if (err) throw err;
-  else console.log("The magic happened!");
-});
+if (args[1] === "-s") {
+  serverGenerator(args[0], (err) => {
+    if (err) throw err;
+    else console.log(`${args[0]} server files created - run npm install!`);
+  });
+} else {
+  projectGenerator(args[0], (err) => {
+    if (err) throw err;
+    else console.log(`${args[0]} files created - run npm install!`);
+  });
+}
