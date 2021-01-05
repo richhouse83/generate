869edc3e2e69b2fcbb496652bb034db39aaff834
Rich House <rich@richhouse.co.uk>
2021-01-05 20:43:02 +0000

    promisifying begun

--- bin/cli.js
+++ bin/cli.js
@@ -6,10 +6,13 @@ const serverGenerator = require("../lib/server-generator");
 const [, , ...args] = process.argv;
 
 if (args[1] === "-s") {
-  serverGenerator(args[0], (err) => {
-    if (err) throw err;
-    else console.log(`${args[0]} server files created`);
-  });
+  return serverGenerator(args[0])
+    .then(() => {
+      console.log("Success! Now run npm install.");
+    })
+    .catch((err) => {
+      throw Error(err);
+    });
 } else {
   projectGenerator(args[0], (err) => {
     if (err) throw err;
