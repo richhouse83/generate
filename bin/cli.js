67eb1d76b0f1fd6d16cd5f1f09943f7f444688bd
Rich House <rich@richhouse.co.uk>
2021-01-06 18:56:40 +0000

    promisifying complete

--- bin/cli.js
+++ bin/cli.js
@@ -1,21 +1,31 @@
 #!/usr/bin/env node
 
+const colors = require("colors");
 const projectGenerator = require("../lib/project-generator");
 const serverGenerator = require("../lib/server-generator");
 
 const [, , ...args] = process.argv;
 
 if (args[1] === "-s") {
+  console.log("\nCreating Server File Structure...\n\n".yellow);
+
   return serverGenerator(args[0])
     .then(() => {
-      console.log("Success! Now run npm install.");
+      console.log("Success! Now run npm install.".green);
+      return;
     })
     .catch((err) => {
       throw Error(err);
     });
 } else {
-  projectGenerator(args[0], (err) => {
-    if (err) throw err;
-    else console.log(`${args[0]} files created `);
-  });
+  console.log("\nCreating Generic File Structure...\n\n\n".yellow);
+
+  return projectGenerator(args[0])
+    .then(() => {
+      console.log(`Success! ${args[0]} created. `.green);
+      return;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
